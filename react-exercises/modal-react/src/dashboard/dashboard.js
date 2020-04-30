5da64c9ac80cf95e3892d0b48176de57f996f787
abalajikamalesh <abalajikamalesh@gmail.com>
2020-04-30 17:11:48 +0530

    Add dashboard and reach/router

---
+++ react-exercises/modal-react/src/dashboard/dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './dashboard.css';
+
+export default (props) => {
+  return(
+    <div>
+        <img src="https://cdn3.vectorstock.com/i/1000x1000/50/82/infographic-dashboard-template-with-flat-design-vector-22465082.jpg" alt="W3Schools.com"/>
+    </div>
+  )
+}
--- react-exercises/modal-react/src/index.js
+++ react-exercises/modal-react/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "@reach/router";
 import "./index.css";
 import App from "./App";
+import Dashboard from './dashboard/dashboard';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <App path="/"/>
+    <Dashboard path="/dashboard" />
+   </Router>,
   document.getElementById("root")
 );
