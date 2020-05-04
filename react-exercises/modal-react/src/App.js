6bf8fb51fdc8e826622c7dfb8c00ba0e6196bacc
abalajikamalesh <abalajikamalesh@gmail.com>
2020-05-05 01:05:26 +0530

    Refactor App.js

--- react-exercises/modal-react/src/App.js
+++ react-exercises/modal-react/src/App.js
@@ -29,58 +29,44 @@ function App() {
       <button id="signup" className="button" onClick={() => { setActiveModal('signup')}}>Sign up</button>
       <button id="default" className="button" onClick={() => { setActiveModal('default')}}>Default</button>
 
-      { activeModal === 'login' &&  
-                      <Modal 
-                      title="Login" 
-                      confirmActionName="Login"
-                      backDropClassName="backdrop"
-                      onBackDropClick={close} 
-                      onCancel={close}
-                      onConfirm={redirect}>
-                        <form>
-                          <div className="form-input">
-                            <label htmlFor="username">Username</label>
-                            <input type="text" id="username" placeholder=""/>
-                          </div>
-                          <div className="form-input">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" id="password" placeholder=""/>
-                          </div>
-                          <div className="form-input">
-                            <label htmlFor="remember">Remember Me</label>
-                            <input type="checkbox" id="remember"/>
-                          </div>
-                        </form>
-                      </Modal>}
+      { activeModal === 'login' &&  <Modal title="Login" confirmActionName="Login" backDropClassName="backdrop"
+                                           onBackDropClick={close} onCancel={close} onConfirm={redirect}>
+                                        <form>
+                                          <div className="form-input">
+                                            <label htmlFor="username">Username</label>
+                                            <input type="text" id="username" placeholder=""/>
+                                          </div>
+                                          <div className="form-input">
+                                            <label htmlFor="password">Password</label>
+                                            <input type="password" id="password" placeholder=""/>
+                                          </div>
+                                          <div className="form-input">
+                                            <label htmlFor="remember">Remember Me</label>
+                                            <input type="checkbox" id="remember"/>
+                                          </div>
+                                        </form>
+                                    </Modal>}
 
-      { activeModal === 'signup' &&
-                        <Modal 
-                        title="Signup" 
-                        confirmActionName="Sign Up" 
-                        backDropClassName="backdrop" 
-                        modalStyleClassName={myModalStyle}
-                        onBackDropClick={close}
-                        onCancel={close} 
-                        onConfirm={redirect}>
-                        <form>
-                          <div className="form-input">
-                            <label htmlFor="username">Username</label>
-                            <input type="text" id="username" placeholder=""/>
-                          </div>
-                          <div className="form-input">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" id="password" placeholder=""/>
-                          </div>
-                          <div className="form-input">
-                            <label htmlFor="repassword">Re enter Password</label>
-                            <input type="password" id="repassword" placeholder=""/>
-                          </div>
-                        </form>
-                      </Modal>}
+      { activeModal === 'signup' && <Modal title="Signup" confirmActionName="Sign Up" backDropClassName="backdrop" 
+                                           modalStyleClassName={myModalStyle} onBackDropClick={close} onCancel={close} 
+                                           onConfirm={redirect}>
+                                        <form>
+                                          <div className="form-input">
+                                            <label htmlFor="username">Username</label>
+                                            <input type="text" id="username" placeholder=""/>
+                                          </div>
+                                          <div className="form-input">
+                                            <label htmlFor="password">Password</label>
+                                            <input type="password" id="password" placeholder=""/>
+                                          </div>
+                                          <div className="form-input">
+                                            <label htmlFor="repassword">Re enter Password</label>
+                                            <input type="password" id="repassword" placeholder=""/>
+                                          </div>
+                                        </form>
+                                    </Modal>}
           
-        { activeModal === 'default' && 
-                            <Modal onCancel={close}></Modal>
-        }
+        { activeModal === 'default' && <Modal onCancel={close}></Modal>}
     </div>
   );
 }
