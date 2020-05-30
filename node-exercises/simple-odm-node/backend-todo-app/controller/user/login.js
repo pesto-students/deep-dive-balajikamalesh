ac99ac3bb67fe73e08bce09ee7a3ad6c24ac4e76
abalajikamalesh <abalajikamalesh@gmail.com>
2020-05-30 18:30:44 +0530

    Add user login and signup functionality

--- node-exercises/simple-odm-node/backend-todo-app/controller/user/login.js
+++ node-exercises/simple-odm-node/backend-todo-app/controller/user/login.js
@@ -1,10 +1,14 @@
 const { User } = require('../../models');
 
 async function login({ request, response }) {
-  //
-  // insertOne sample
-  let result = await User.insertOne({});
-  response.json({ status: 200, result: 'OK' });
+  let query = { username: request.body.username, password: request.body.password }
+  let user = await User().find({ query });
+  
+  if(user.length > 0){
+    response.json({ status: 200, result: 'OK' });
+  } else {
+    response.json({ status: 404, result: 'User not found' });
+  }
 }
 
 module.exports = { login };
