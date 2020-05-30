fc53100f1adb39ba4a10e44cc88e0bb75788846f
abalajikamalesh <abalajikamalesh@gmail.com>
2020-05-30 19:54:57 +0530

    Sign in, signup implemented

--- node-exercises/simple-odm-node/frontend-todo-app/todo/src/components/SignIn.js
+++ node-exercises/simple-odm-node/frontend-todo-app/todo/src/components/SignIn.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -13,6 +11,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { navigate } from '@reach/router';
+import axios from 'axios';
 
 function Copyright() {
   return (
@@ -49,9 +48,20 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignIn() {
   const classes = useStyles();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   let signin = () => {
-      
+    axios({
+      method: 'post',
+      url: 'http://localhost:8000/login',
+      data: {
+        username: username,
+        password: password
+      }
+    }).then(res => {
+      navigate('/todo', { state: { fromApp: true }})
+    })
   }
 
   return (
@@ -70,10 +80,10 @@ export default function SignIn() {
             margin="normal"
             required
             fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
+            id="username"
+            label="User Name"
+            name="username"
+            onChange={(e) => setUsername(e.target.value)}
             autoFocus
           />
           <TextField
@@ -85,6 +95,7 @@ export default function SignIn() {
             label="Password"
             type="password"
             id="password"
+            onChange={(e) => setPassword(e.target.value)}
             autoComplete="current-password"
           />
           {/* <FormControlLabel
--- node-exercises/simple-odm-node/frontend-todo-app/todo/src/components/SignUp.js
+++ node-exercises/simple-odm-node/frontend-todo-app/todo/src/components/SignUp.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -13,6 +11,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { navigate } from '@reach/router';
+import axios from 'axios';
 
 function Copyright() {
   return (
@@ -49,9 +48,20 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignUp() {
   const classes = useStyles();
-  
-  let signup = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
+  let signup = () => {
+    axios({
+      method: 'post',
+      url: 'http://localhost:8000/signup',
+      data: {
+        username: username,
+        password: password
+      }
+    }).then(res => {
+      navigate('/todo', { state: { fromApp: true }})
+    })
   }
     
   return (
@@ -94,9 +104,10 @@ export default function SignUp() {
                 variant="outlined"
                 required
                 fullWidth
-                id="email"
-                label="Email Address"
+                id="username"
+                label="User Name"
                 name="email"
+                onChange={(e) => setUsername(e.target.value)}
                 autoComplete="email"
               />
             </Grid>
@@ -109,6 +120,7 @@ export default function SignUp() {
                 label="Password"
                 type="password"
                 id="password"
+                onChange={(e) => setPassword(e.target.value)}
                 autoComplete="current-password"
               />
             </Grid>
